b357bbf55def18eeb6c93dc62c0605ac2e3fa2c0
Nate Goldsborough <nathen418@playantares.com>
2022-03-30 09:42:49 -0500

    Update initial-setup-ubuntu.sh

--- initial-setup-ubuntu.sh
+++ initial-setup-ubuntu.sh
@@ -2,25 +2,42 @@
 
 # This script is acessable at this link: https://playantares.com/scripts/initial-setup-ubuntu.sh
 
+# Update & Upgrade
 sudo apt update
 sudo apt upgrade -y
-sudo apt install unattended-upgrades -y
+
+# Install common packages
+sudo apt install openssh-server git speedtest-cli fail2ban unattended-upgrades glances -y
+sudo snap install bashtop
+
+# Reconfigure unattended-upgrades
 sudo dpkg-reconfigure --priority=low unattended-upgrades
+
+# Disable root login
 sudo passwd -l root
-sudo apt install openssh-server -y
+
+# Disable password login over ssh
 sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]yes/c\PasswordAuthentication no" /etc/ssh/sshd_config
 sudo service sshd restart
+
+# Extend ubuntu default fs to fill the disk
 sudo lvextend -l +100%FREE /dev/ubuntu-vg/ubuntu-lv
 sudo resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
+
+# Change the default time zone
 sudo timedatectl set-timezone America/Chicago
+
+# Set basic firewall rules
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow ssh
 echo "y" | sudo ufw enable
-sudo apt install fail2ban -y
+
+# Make backup and configure fail2ban
 sudo cp /etc/fail2ban/fail2ban.{conf,local}
 sudo cp /etc/fail2ban/jail.{conf,local}
 sudo sed -i "/^[^#]*backend = auto/c\backend = systemd" /etc/fail2ban/jail.local
 sudo systemctl restart fail2ban
-sudo apt install git speedtest-cli glances -y
-sudo snap install bashtop
+
+# Exit script
+echo "Script completed!"
