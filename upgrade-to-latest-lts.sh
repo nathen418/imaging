57ddc69485b5b805e47e0fd9dbe2aac25a8a8b6c
Nate Goldsborough <nathen418@playantares.com>
2022-05-24 18:23:33 -0500

    Update upgrade-to-latest-lts.sh

--- upgrade-to-latest-lts.sh
+++ upgrade-to-latest-lts.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# This script is acessable at this link: https://playantares.com/scripts/upgrade-to-latest-lts.sh
-
+# This script is acessable at this link: https://playantares.com/scripts/up>
+echo -e "\e[31mTHIS SCRIPT REQUIRES YOU TO ANSWER PROMPTS, IT IS NOT UNATENDED!. Press any key to continue.\e[0m"
+read -n 1 -s
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 sudo apt-get autoremove -y
 sudo apt install update-manager-core
 sudo do-release-upgrade -d
-cat /etc/lsb-release
+echo -e "\e[31mTHIS SCRIPT WILL REBOOT YOUR COMPUTER. PRESS ANY KEY TO CONTINUE!\e[0m"
+read -n 1 -s
+sudo reboot
