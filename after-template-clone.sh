328ca1e1ff39705f42b408165b11c155d2022425
Nate Goldsborough <nathen418@playantares.com>
2023-04-30 19:58:48 -0500

    Fix my stupidity

--- after-template-clone.sh
+++ after-template-clone.sh
@@ -13,8 +13,13 @@ read -p "What should the hostname be: " hostname
 sudo hostnamectl set-hostname "$hostname"
 
 echo "Setting up JumpCloud on this device"
-read -p "What is the jumpcoud connection id (x-connect-key)?" x-connect-key
-curl --tlsv1.2 --silent --show-error --header 'x-connect-key: "$x-connect-key"' https://kickstart.jumpcloud.com/Kickstart | sudo bash
+read -p "What is the JumpCloud connection id (x-connect-key)?" key
+header="x-connect-key: $key"
+
+wget -q --header "$header" https://kickstart.jumpcloud.com/Kickstart
+echo "Running the JumpCloud Install Script"
+bash Kickstart
 
 read -p "This machine needs to reboot. Is that OK? (y/n)"  confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+sudo rm Kickstart
 sudo reboot
